353beb814ef233209182d758ab96f2ef7b71eba8
zk <596718356@qq.com>
2019-12-13 22:48:31 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,60 +1,121 @@
-import React, {  Component, PureComponent, memo } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import './App.css';
 
-// memo文档
-// https://zh-hans.reactjs.org/docs/react-api.html#reactmemo
-// React.PureComponent 中的 shouldComponentUpdate() 仅作对象的浅层比较。如果对象中包含复杂的数据结构，则有可能因为无法检查深层的差别，产生错误的比对结果。仅在你的 props 和 state 较为简单时，才使用 React.PureComponent，或者在深层数据结构发生变化时调用 forceUpdate() 来确保组件被正确地更新。你也可以考虑使用 immutable 对象加速嵌套数据的比较
+// hooks 出现之前 类组件不足
+// 1 缺少复用机制，状态逻辑难以复用
+  // 渲染属性和高阶组件导致层级冗余
+// 2 相关逻辑复杂难以维护
+// 3 this 指向困扰
+// hooks优势
+// 1 函数组件无this问题
+// 2 自定义hook方便复用状态逻辑
+// 3 副作用的关注点分离
 
-// class Foo extends PureComponent {
-//   // shouldComponentUpdate (nextProps, nextState) {
-//   //   if (nextProps.name === this.props.name) {
-//   //     return false
-//   //   }
-//   //   return true
-//   // }
-
-//   // 无状态组件
-//   // PureComponent 只有props 的第一级发生变化才会更新
-//   render () {
-//     console.log("foo render");
-//     return (
-//       <div>
-//           {this.props.person.age}
-//       </div>
-//     )
-//   }
-// }
-const Foo = memo(function Foo (props) {
-  console.log("Foo render");
-  return (
-      <div>
-          {props.person.age}
-      </div>
-    )
-})
-
-class App extends Component {
+// npm i -D eslint-plugin-react-hooks
+// 配置 package.json
+/**
+ * { eslintConfig:{
+ *   "plugins": [react-hooks]
+ *   "react-hooks": {
+ *     "react-hooks/rules-of-hooks": "error"
+ *   }
+ * }}
+ */
+class App2 extends Component {
   state = {
-    count: 1,
-    person: {
-      age: 1
+    count: 0,
+    size: {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight
     }
   }
-  callback = () => {
+  // 用类属性声明，可以保证this的正确指向
+  onResize = () => {
+    this.setState({
+      size: {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
+      }
+    })
+  }
 
+  componentDidMount(){
+    document.title = this.state.count
+    window.addEventListener('resize', this.onResize, false)
+  }
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.onResize, false)
+  }
+  componentDidUpdate(){
+    document.title = this.state.count
   }
   render () {
-    const { count, person } = this.state
+    const { count, size } = this.state
     return (
-      <div>
-          <button onClick={() => {
-            person.age++
-            this.setState({count: count + 1})}
-          }>按钮</button>
-          <Foo person={person} cb={ this.callback }/>
-          <div>{this.state.person.age}</div>
-      </div>
+      <button onClick={() => {this.setState({count: count + 1})}}>
+        Click Me { count }
+        size: {size.width} * {size.height}
+      </button>
     )
   }
+}
+
+function App (props) {
+  // useState可以传入一个参数，延时初始化默认值
+  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(() => {
+    console.log("initial count");
+    return props.defaultCount || 0
+  })
+
+  const [size, setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight
+  })
+
+  const onResize = () => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight
+    })
+  }
+
+  useEffect(() => {
+    document.title = count
+  })
+
+  // useEffect 文档
+  // http://react.html.cn/docs/hooks-effect.html
+  useEffect(() => {
+    window.addEventListener('resize', onResize, false)
+    
+    // return 一个回调函数，一在组件卸载时候调用二在组件重新宣软是调用
+    return () => {
+      window.removeEventListener('resize', onResize, false)
+    }
+  }, [])
+  // useEffect第二个参数，传入空数组相当于 第一次渲染为空数组，第二次还是空数组，两者相等所以第二次不执行，整个useEffect 只执行一次
+  // 在某些情况下，在每次渲染后清理或应用 effect可能会产生性能问题。在类组件中，我们可以通过在componentDidUpdate 中编写与 prevProps 或 prevState 的额外比较来解决这个问， 
+  // 例如
+  useEffect(() => {
+    console.log('count:'+count);
+  }, [count])
+
+  // 在dom元素上绑定事件
+  const click = () => {
+    console.log('click');
+  }
+  useEffect(() => {
+    document.querySelector('.size').addEventListener("click", click, false)
+  }, [])
+
+  return (
+    <div>
+      <button onClick={() => {setCount(count+1)}}>
+        Click me {count}
+      </button>
+      <p className="size">size: {size.width} * {size.height}</p>
+    </div>
+  )
 }
 export default App;
